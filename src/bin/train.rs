02a1209188b9a19d89320f10edc3c08c988aa328
Alexandre Bury <alexandre.bury@gmail.com>
2016-02-22 22:42:42 -0800

    Add comments

--- src/bin/train.rs
+++ src/bin/train.rs
@@ -5,20 +5,32 @@ use std::fs;
 use std::str::FromStr;
 use std::io::{self, Read, Write};
 
+// This program trains a dictionary from one or more files,
+// to make future compression of similar small files more efficient.
+//
+// The dictionary will need to be present during decompression,
+// but it you need to compress many small files individually,
+// it may be worth the trouble.
 fn main() {
+    // We will concatenate all the input files in one big buffer.
     let mut buffer = Vec::new();
+    // We'll keep track of each file size
     let mut sample_sizes = Vec::new();
 
+    // Passing "--" treats everything after that as a filename
     let mut ignore_options = false;
-    let mut max_size = 110 * 1024;
 
+    // A tiny state machine to parse the max size.
     let mut expect_size = false;
+    let mut max_size = 110 * 1024;
 
     if env::args().len() == 1 {
         writeln!(io::stderr(), "Usage: `train [-c MAX_SIZE] FILES...`").unwrap();
         return;
     }
 
+    // We do some ugly manual option parsing, because I'm trying to avoid using `clap`.
+    // When cargo allows per-binary dependencies, this may get cleaner.
     for filename in env::args().skip(1) {
         if !ignore_options {
             match filename.as_ref() {

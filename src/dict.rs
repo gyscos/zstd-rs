8cf809af607edc68d3dfd91cbf1ba1cf7fdf7154
Alexandre Bury <alexandre.bury@gmail.com>
2016-02-24 17:43:14 -0800

    Rename `train_dict()` -> `dict::from_continuous()`
    * Also add `dict::from_samples()`
    * Add some doc

---
+++ src/dict.rs
@@ -0,0 +1,58 @@
+//! Train a dictionary from various sources.
+//!
+//! A dictionary can help improve the compression of small files.
+//! The dictionary must be present during decompression,
+//! but can be shared accross multiple "similar" files.
+//!
+//! Creating a dictionary using the `zstd` C library,
+//! using the `zstd` command-line interface, using this library,
+//! or using the `train` binary provided, should give the same result,
+//! and are therefore completely compatible.
+//!
+//! To use, see [`Encoder::with_dictionary`] or [`Decoder::with_dictionary`].
+//!
+//! [`Encoder::with_dictionary`]: ../struct.Encoder.html#method.with_dictionary
+//! [`Decoder::with_dictionary`]: ../struct.Decoder.html#method.with_dictionary
+
+use std::io;
+use ll;
+
+/// Train a dictionary from a big continuous chunk of data.
+pub fn from_continuous(sample_data: &[u8],
+                       sample_sizes: &[usize],
+                       max_size: usize)
+                       -> io::Result<Vec<u8>> {
+    // Complain if the lengths don't add up to the entire data.
+    if sample_sizes.iter().fold(0, |a, b| a + b) != sample_data.len() {
+        return Err(io::Error::new(io::ErrorKind::Other,
+                                  "sample sizes don't add up".to_string()));
+    }
+
+    let mut result = Vec::with_capacity(max_size);
+    unsafe {
+        let code = ll::ZDICT_trainFromBuffer(result.as_mut_ptr(),
+                                             result.capacity(),
+                                             sample_data.as_ptr(),
+                                             sample_sizes.as_ptr(),
+                                             sample_sizes.len());
+        let written = try!(ll::parse_code(code));
+        result.set_len(written);
+    }
+    Ok(result)
+}
+
+/// Train a dictionary from multiple samples.
+///
+/// The samples will internaly be copied to a single continuous buffer,
+/// so make sure you have enough memory available.
+pub fn from_samples<S: AsRef<[u8]>>(samples: &[S],
+                                    max_size: usize) -> io::Result<Vec<u8>> {
+    // Copy every sample to a big chunk of memory
+    let data: Vec<_> = samples.iter()
+                              .flat_map(|s| s.as_ref())
+                              .map(|&b| b)
+                              .collect();
+    let sizes: Vec<_> = samples.iter().map(|s| s.as_ref().len()).collect();
+
+    from_continuous(&data, &sizes, max_size)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -25,44 +25,21 @@ extern crate libc;
 mod ll;
 mod encoder;
 mod decoder;
+pub mod dict;
 
 pub use encoder::Encoder;
 pub use decoder::Decoder;
 
 use std::io;
 
-/// Trains a dictionary to improve compression of small files.
-///
-/// The dictionary must be present during decompression,
-/// but can be shared accross multiple "similar" files.
-pub fn train_dict(sample_data: &[u8],
-                  sample_sizes: &[usize],
-                  max_size: usize)
-                  -> io::Result<Vec<u8>> {
-    // Complain if the lengths don't add up to the entire data.
-    if sample_sizes.iter().fold(0, |a, b| a + b) != sample_data.len() {
-        return Err(io::Error::new(io::ErrorKind::Other,
-                                  "sample sizes don't add up".to_string()));
-    }
-
-    let mut result = Vec::with_capacity(max_size);
-    unsafe {
-        let code = ll::ZDICT_trainFromBuffer(result.as_mut_ptr(),
-                                             result.capacity(),
-                                             sample_data.as_ptr(),
-                                             sample_sizes.as_ptr(),
-                                             sample_sizes.len());
-        let written = try!(ll::parse_code(code));
-        result.set_len(written);
-    }
-    Ok(result)
-}
 
 #[test]
 fn test_cycle() {
     use std::io::{Read, Write};
-    let text = "This is a sample text. It is not meant to be interesting or anything. Just text, \
-                nothing more. Don't expect too much from it.";
+    let text = "This is a sample text. \
+                It is not meant to be interesting or anything. \
+                Just text, nothing more. \
+                Don't expect too much from it.";
 
     let mut buffer: Vec<u8> = Vec::new();
 

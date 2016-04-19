d4a9d1e60643b8003dfd6e95b88abba010798124
Alexandre Bury <alexandre.bury@gmail.com>
2016-04-18 19:03:11 -0700

    Update to zstd 0.6.1
    With new lib file layout
    Also add -O3 to compilation options.

--- src/build.rs
+++ src/build.rs
@@ -1,12 +1,31 @@
 extern crate gcc;
 
 fn main() {
-    gcc::compile_library("libzstd.a",
-                         &["zstd/lib/zstd_compress.c",
-                           "zstd/lib/zstd_decompress.c",
-                           "zstd/lib/fse.c",
-                           "zstd/lib/huff0.c",
-                           "zstd/lib/zdict.c",
-                           "zstd/lib/divsufsort.c",
-                           "zstd/lib/zbuff.c"]);
+    let mut config = gcc::Config::new();
+
+    let source_files = &["zstd/lib/compress/zstd_compress.c",
+                         "zstd/lib/compress/fse_compress.c",
+                         "zstd/lib/compress/huf_compress.c",
+                         "zstd/lib/compress/zbuff_compress.c",
+
+                         "zstd/lib/decompress/zstd_decompress.c",
+                         "zstd/lib/common/fse_decompress.c",
+                         "zstd/lib/decompress/huf_decompress.c",
+                         "zstd/lib/decompress/zbuff_decompress.c",
+
+                         "zstd/lib/dictBuilder/zdict.c",
+                         "zstd/lib/dictBuilder/divsufsort.c",
+
+                         "zstd/lib/common/zstd_common.c",
+                         "zstd/lib/common/entropy_common.c"];
+    for file in source_files {
+        config.file(file);
+    }
+
+    // Some extra parameters
+    config.opt_level(3);
+    config.include("zstd/lib/common");
+
+    // Compile!
+    config.compile("libzstd.a");
 }

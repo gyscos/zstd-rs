247143909de012c3ead724948b10817bd3c9f256
Alexandre Bury <alexandre.bury@gmail.com>
2016-05-29 11:36:41 +0200

    Update for zstd 0.7

--- src/build.rs
+++ src/build.rs
@@ -1,25 +1,19 @@
 extern crate gcc;
+extern crate glob;
 
 fn main() {
     let mut config = gcc::Config::new();
 
-    let source_files = &["zstd/lib/compress/zstd_compress.c",
-                         "zstd/lib/compress/fse_compress.c",
-                         "zstd/lib/compress/huf_compress.c",
-                         "zstd/lib/compress/zbuff_compress.c",
+    let globs = &["zstd/lib/common/*.c",
+                  "zstd/lib/compress/*.c",
+                  "zstd/lib/decompress/*.c",
+                  "zstd/lib/dictBuilder/*.c"];
 
-                         "zstd/lib/decompress/zstd_decompress.c",
-                         "zstd/lib/common/fse_decompress.c",
-                         "zstd/lib/decompress/huf_decompress.c",
-                         "zstd/lib/decompress/zbuff_decompress.c",
-
-                         "zstd/lib/dictBuilder/zdict.c",
-                         "zstd/lib/dictBuilder/divsufsort.c",
-
-                         "zstd/lib/common/zstd_common.c",
-                         "zstd/lib/common/entropy_common.c"];
-    for file in source_files {
-        config.file(file);
+    for pattern in globs {
+        for path in glob::glob(pattern).unwrap() {
+            let path = path.unwrap();
+            config.file(path);
+        }
     }
 
     // Some extra parameters

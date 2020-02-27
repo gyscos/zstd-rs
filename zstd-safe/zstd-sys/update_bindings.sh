0a7e0af423f2821e016ddef043629b049d88d7ed
Josh Stone <jistone@redhat.com>
2020-02-27 15:33:57 -0800

    Upgrade bindgen to 0.53.1

--- zstd-safe/zstd-sys/update_bindings.sh
+++ zstd-safe/zstd-sys/update_bindings.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-bindgen zstd.h --ctypes-prefix ::libc --blacklist-type max_align_t --rustified-enum '.*' --use-core -o src/bindings.rs -- -Izstd/lib
-bindgen zstd.h --ctypes-prefix libc --blacklist-type max_align_t --rustified-enum '.*' --use-core -o src/bindings_experimental.rs -- -Izstd/lib -DZSTD_STATIC_LINKING_ONLY
-bindgen zstd.h --blacklist-type max_align_t --rustified-enum '.*' --use-core -o src/bindings_std.rs -- -Izstd/lib
-bindgen zstd.h --blacklist-type max_align_t --rustified-enum '.*' --use-core -o src/bindings_std_experimental.rs -- -Izstd/lib -DZSTD_STATIC_LINKING_ONLY
+bindgen="bindgen --blacklist-type=max_align_t --size_t-is-usize --rustified-enum=.* --use-core"
+
+$bindgen zstd.h --ctypes-prefix ::libc -o src/bindings.rs -- -Izstd/lib
+$bindgen zstd.h --ctypes-prefix libc -o src/bindings_experimental.rs -- -Izstd/lib -DZSTD_STATIC_LINKING_ONLY
+$bindgen zstd.h -o src/bindings_std.rs -- -Izstd/lib
+$bindgen zstd.h -o src/bindings_std_experimental.rs -- -Izstd/lib -DZSTD_STATIC_LINKING_ONLY

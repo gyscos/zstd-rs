f5a7f389665161f024369ff4f209211b9193748b
Alexandre Bury <alexandre.bury@gmail.com>
2022-01-27 09:27:58 -0500

    Split binding generation into zstd and zdict (#141)

--- zstd-safe/zstd-sys/zdict.h
+++ zstd-safe/zstd-sys/zdict.h
@@ -1,12 +1,12 @@
 #ifdef PKG_CONFIG
 
 /* Just use installed headers */
-#include <zstd.h>
+#include <zdict.h>
 // Don't use experimental features like zstdmt
 
 #else // #ifdef PKG_CONFIG
 
-#include "zstd/lib/zstd.h"
+#include "zstd/lib/zdict.h"
 
 #endif // #ifdef PKG_CONFIG
 
@@ -14,3 +14,4 @@
 /* This file is used to generate bindings for both headers.
  * Check update_bindings.sh to see how to use it.
  * Or use the `bindgen` feature, which will create the bindings automatically. */
+

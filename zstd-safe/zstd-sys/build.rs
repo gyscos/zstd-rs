d4916c836bc91aac05293cd3448768cc84470cc0
Oliver Schneider <oschneider@exocad.com>
2025-11-07 16:55:39 +0100

    Only set -fvisibility=hidden if not using MSVC

--- zstd-safe/zstd-sys/build.rs
+++ zstd-safe/zstd-sys/build.rs
@@ -210,7 +210,9 @@ fn compile_zstd() {
     // Hide symbols from resulting library,
     // so we can be used with another zstd-linking lib.
     // See https://github.com/gyscos/zstd-rs/issues/58
-    config.flag("-fvisibility=hidden");
+    if ! cfg!(target_env = "msvc") {
+        config.flag("-fvisibility=hidden");
+    }
     config.define("XXH_PRIVATE_API", Some(""));
     config.define("ZSTDLIB_VISIBILITY", Some(""));
     #[cfg(feature = "zdict_builder")]
